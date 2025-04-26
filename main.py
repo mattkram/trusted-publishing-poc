7a17aca005efe1f7308bbaebc0248fa92f260029
mattkram <mkramer@anaconda.com>
2025-04-25 23:01:54 -0500

    feat: Start implementing token validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, HTTPException, status
 
 OIDC_CONFIG_URL = (
     "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
@@ -14,11 +14,24 @@ async def home() -> dict:
     return {"hello": "world"}
 
 
+async def grant_access_token(id_token: str) -> str:
+    return "super-secret-token"
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Invalid workflow",
+    )
+
+
 @app.get("/token")
 async def get_token(
-    id_token: Optional[str] = Header(default=None, alias="authorization"),
+    authorization: Optional[str] = Header(default=None),
 ) -> dict:
-    return {
-        "access_token": "super-secret-token",
-        "received_header": " ".join(id_token),
-    }
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Must provide a valid id token",
+        )
+
+    _, id_token = authorization.split()
+    access_token = await grant_access_token(id_token)
+    return {"access_token": access_token}
