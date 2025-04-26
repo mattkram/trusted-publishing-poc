c97bbc74cae95f09c0fa2e48ebc8dcf0417abf3f
mattkram <mkramer@anaconda.com>
2025-04-25 22:48:34 -0500

    feat: Add token endpoint

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+from typing import Optional
 
+from fastapi import FastAPI, Header
+
+OIDC_CONFIG_URL = (
+    "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+)
 
 app = FastAPI()
 
@@ -7,3 +12,13 @@ app = FastAPI()
 @app.get("/")
 async def home() -> dict:
     return {"hello": "world"}
+
+
+@app.get("/token")
+async def get_token(
+    id_token: Optional[str] = Header(default=None, alias="authorization"),
+) -> dict:
+    return {
+        "access_token": "super-secret-token",
+        "received_header": " ".join(id_token),
+    }
