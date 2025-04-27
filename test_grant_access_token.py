faea5533b05fb416ab8e2b7e87a284ce106c5073
mattkram <mkramer@anaconda.com>
2025-04-26 23:27:51 -0500

    tidy: Remove pytest stuff

--- test_grant_access_token.py
+++
@@ -1,9 +0,0 @@
-from main import grant_access_token
-
-TEST_TOKEN = "TEST_TOKEN"
-
-
-async def test_grant_access_token():
-    access_token = await grant_access_token(TEST_TOKEN)
-
-    assert access_token == "super-secret-token"
